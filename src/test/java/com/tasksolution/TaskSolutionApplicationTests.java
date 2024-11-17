9626bca310b29482bed4ed6fffab9748e19740f5
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-11-17 11:45:35 +0100

    Initial commit

---
+++ src/test/java/com/tasksolution/TaskSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasksolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
